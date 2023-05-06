44f043fa1feae7aaeced0d27061dfbed0ada818d
Max V. Shumakov <shumakovmv@gmail.com>
2023-05-06 15:30:49 +0300

    iWork-05 3.1 form 1 to 5

---
+++ src/components/SidePanel.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-card class="my-card">
+    <q-item>
+      <q-item-section avatar>
+        <q-avatar>
+          <img src="~/assets/mvs013.svg">
+        </q-avatar>
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>{{ ownersTitle }}</q-item-label>
+        <q-item-label caption>{{ ownersSubTitle }}</q-item-label>
+      </q-item-section>
+    </q-item>
+
+    <img src="https://cdn.quasar.dev/img/parallax2.jpg"/>
+  </q-card>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SidePanel',
+
+  setup () {
+    return {
+      ownersTitle: 'Max V. Shumakov',
+      ownersSubTitle: 'This is my todo list'
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,8 @@
 <template>
   <q-page class="flex flex-center">
     <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
+      alt="MVS"
+      src="~assets/mvs013.svg"
       style="width: 200px; height: 200px"
     >
   </q-page>
